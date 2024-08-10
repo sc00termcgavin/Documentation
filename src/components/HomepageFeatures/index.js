36b08ab262db4ae8767ed2d7a07992205326832b
sc00termcgavin <suspiciously.unknown@pm.me>
2024-08-10 14:07:10 -0600

    landing page changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'About Me',
+    link: "docs/category/index",
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        About Me
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
+    link: "docs/category/comptia",
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        compTIA security+ Notes
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Notes',
+    link: "docs/category/notes",
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        notes
       </>
     ),
   },
