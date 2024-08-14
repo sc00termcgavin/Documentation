1ea41f9d52a91d50174ff639d680d9c722f582b1
sc00termcgavin <suspiciously.unknown@pm.me>
2024-08-14 13:33:50 -0600

    update homepage css

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,35 +5,35 @@ import Link from "@docusaurus/Link";
 
 const FeatureList = [
   {
-    title: 'About Me',
-    link: "/docs/category/notes",
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'CompTIA',
+    link: "/docs/category/comptia",
+    Svg: require('@site/static/img/security.svg').default,
     description: (
       <>
-        About Me
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    link: "docs/category/comptia",
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        compTIA security+ Notes
+        CompTIA Security+ Notes
       </>
     ),
   },
   {
     title: 'Notes',
     link: "docs/category/notes",
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/notes.svg').default,
     description: (
       <>
-        notes
+        HomeLab Configuration Notes
       </>
     ),
   },
+  // {
+  //   title: 'Notes',
+  //   link: "docs/category/notes",
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       notes
+  //     </>
+  //   ),
+  // },
 ];
 
 function Feature({Svg, title, description, link}) {
@@ -54,7 +54,9 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        {/* <div className="row"> */}
+        <div className={clsx('row', styles.row)}>
+        {/* FeatureList.slice(0,2).map */}
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
